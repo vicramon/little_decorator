a8a57058f2c69995c7c80498ba989a8833ad577e
Shaun Hubbard <shaunhubbard2013@icloud.com>
2014-06-26 17:23:10 -0400

    I should have added all the files in the last quit

---
+++ spec/lib/generators/generators_feature_spec.rb
@@ -0,0 +1,54 @@
+require 'rails'
+
+class Application < ::Rails::Application; end
+
+require 'pry_debug'
+require 'ammeter/init'
+require 'generators/interior_decorator_generator'
+
+def pry_in
+  PryDebug.pry
+end
+TMP_ROOT = Pathname.new(Dir.tmpdir)
+
+
+RSpec.configure do |config|
+
+  real_world = nil
+  config.before(:each) do
+    real_world = RSpec.world
+    RSpec.instance_variable_set(:@world, RSpec::Core::World.new)
+  end
+  config.after(:each) do
+    RSpec.instance_variable_set(:@world, real_world)
+  end
+end
+
+describe "In order to easily generate decorator objects" +
+        "As a user of Rails and Interior Decorator" +
+        "I would like to be able to use interior_decorator generators" do
+  it "should pass smoke test" do
+    expect(true).to eql true
+  end
+
+  describe InteriorDecorators::Generators::InteriorDecoratorGenerator, type: :generator do
+    destination TMP_ROOT 
+    before { prepare_destination }
+  
+    it "should run all tasks in generator" do
+      gen = generator %w(decorator)
+      gen.should_receive :create_interior_decorator
+      capture(:stdout) { gen.invoke_all }
+    end
+
+    describe "the generated file" do
+      before { run_generator %w(user) }
+      describe "the file" do
+        subject {file('app/decorators/user_decorator.rb') }
+        it { should exist }
+        it { should contain(/class UserDecorator < InteriorDecorator/) }
+        it { should contain(/end/) }
+      end
+    end
+  end
+end

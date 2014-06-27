9a0049f8592dcea3d40d16447280801d50c86e9e
Shaun Hubbard <shaunhubbard2013@icloud.com>
2014-06-27 13:15:59 -0400

    added collection handling and recursion for multi collections

--- lib/helpers/interior_decorator_helper.rb
+++ lib/helpers/interior_decorator_helper.rb
@@ -1,11 +1,21 @@
 module InteriorDecoratorHelper
 
   def decorate(item)
-    klass = item.respond_to?(:each) ? item.first.class : item.class
+    if item.respond_to?(:each) #? item.first.class : item.class
+      decorated = item.collect{|n| decorate_single_item(n)}
+    else
+      decorated = decorate_single_item item
+    end
+    decorated
+  end
+
+  def decorate_single_item(item)
+    return nil if item.nil?
+    return decorate(item) if item.respond_to?(:each)
+    klass = item.class 
     decorator = "#{klass}Decorator".constantize
     decorator.decorate(item)
   end
-
 end
 
 ActionView::Base.send :include, InteriorDecoratorHelper

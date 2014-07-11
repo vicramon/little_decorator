5817b6e00279650c42e4866d719c08003ac85279
Vic Ramon <vic@vicramon.com>
2014-07-11 11:18:30 -0400

    Use railtie to inclue helpers

--- lib/helpers/interior_decorator_helper.rb
+++ lib/helpers/interior_decorator_helper.rb
@@ -1,9 +1,10 @@
 module InteriorDecoratorHelper
+
   def decorate(item)
     klass = Array(item).first.class
     decorator = "#{klass}Decorator".constantize
     decorator.decorate(item)
   end
   alias_method :d, :decorate
+
 end
-ActionView::Base.send :include, InteriorDecoratorHelper
